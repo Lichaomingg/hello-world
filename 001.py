0852b18ac33edfed2f81a3027aa41b690feb7a61
Lichaomingg <54656043+Lichaomingg@users.noreply.github.com>
2020-08-24 11:39:03 +0800

    Add files via upload

---
+++ 001.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python
+# -*- coding: UTF-8 -*-
+
+
+#rows = int(raw_input("请输入列数：")
+
+for i in range(0,6):
+           i += 1
+           print"*"
+
+
+print"=================================================="
+
+
+s1 = 72
+s2 = 85
+number = input()
+r = number
+print(number % r)
